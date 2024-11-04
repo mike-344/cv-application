cb2fd150721c3728aee20c074fb9f76a49e2dba0
Michael Sullivan <sullivanmichael344@gmail.com>
2024-11-04 08:23:32 -0500

    fix: edit sectioninput function

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,9 +1,9 @@
 import FormInput from "./FormInput";
 
-function InputSection({ text, inputSpecs }) {
-    const inputList = inputSpecs.map((specs) => (
-        <FormInput key={specs.inputId} labelText={specs.labelText} inputId={specs.inputId} />
-    ));
+function SectionInput({ text, inputSpecs }) {
+    const inputList = inputSpecs.map((specs) => {
+        return  <FormInput key={specs.inputId} labelText={specs.labelText} inputId={specs.inputId} inputType={specs.inputType}/>
+});
 
     return (
         <fieldset>
@@ -20,7 +20,7 @@ function ContactSection({text = "Contact Information"}){
         {labelText: "Email: ", inputId: "email"}
     ]
     
-     return <InputSection text={text} inputSpecs={inputSpecs} />;
+     return <SectionInput text={text} inputSpecs={inputSpecs} />;
 
 }
 
@@ -28,24 +28,24 @@ function EducationSection({text = "Education Information"}){
     const inputSpecs = [
         {labelText: "School: ", inputId: "school"}, 
         {labelText: "Degree title: ", inputId: "degreeTitle"},
-        {labelText: "Start Year: ", inputId: "educationStartYear"},
-        {labelText: "End Year: ", inputId: "educationEndYear"}
+        {labelText: "Start Year: ", inputId: "educationStartYear", inputType: "date"},
+        {labelText: "End Year: ", inputId: "educationEndYear", inputType: "date"}
     ]
     
-    return <InputSection text={text} inputSpecs={inputSpecs} />;
+    return <SectionInput text={text} inputSpecs={inputSpecs} />;
 
 }
 
 function ExperienceSection({text = "Experience Information"}){
     const inputSpecs = [
-        {labelText: "Company: ", inputId: "school"}, 
-        {labelText: "Position: ", inputId: "degreeTitle"},
-        {labelText: "Responsibilities: ", inputId: "startYear"},
-        {labelText: "Start Year: ", inputId: "workStartYear"},
-        {labelText: "End Year: ", inputId: "workEndYear"}
+        {labelText: "Company: ", inputId: "company"}, 
+        {labelText: "Position: ", inputId: "position"},
+        {labelText: "Responsibilities: ", inputId: "responsibilities"},
+        {labelText: "Start Year: ", inputId: "workStartYear", inputType: "date"},
+        {labelText: "End Year: ", inputId: "workEndYear", inputType: "date"}
     ]
     
-    return <InputSection text={text} inputSpecs={inputSpecs} />;
+    return <SectionInput text={text} inputSpecs={inputSpecs} />;
 
 }
 export  {ContactSection, EducationSection, ExperienceSection} 
