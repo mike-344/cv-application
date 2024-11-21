f5a4ad21cdbcb86ac19a87cad9aa969927669355
Michael Sullivan <sullivanmichael344@gmail.com>
2024-11-20 19:10:42 -0500

    Begin making general section of form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import { General } from "./generalSection";
 
 
 function App() {
   return (
     <>
-     
+     <General />
     </>
   );
 }
---
+++ src/generalSection.jsx
@@ -0,0 +1,16 @@
+function General(){
+
+    return (
+        <>
+        <label htmlFor="">Name:</label>
+        <input type="text" name="" id="" />
+
+        <label htmlFor="">Email:</label>
+        <input type="text" name="" id="" />
+
+        <label htmlFor="">Phone:</label>
+        <input type="text" name="" id="" />
+        </>
+    )
+}
+export{General}
