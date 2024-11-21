ea2ffd0e2f757ba1e4bc1b753bbc27372000c803
Michael Sullivan <sullivanmichael344@gmail.com>
2024-11-20 20:02:28 -0500

    Render form

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,12 +3,45 @@ function Form(){
     return (
         <>
         <fieldset>
+            <legend>General</legend>
             <label htmlFor="">Name:</label>
             <input type="text" name="" id="" />
+
             <label htmlFor="">Email:</label>
             <input type="text" name="" id="" />
+
             <label htmlFor="">Phone:</label>
             <input type="text" name="" id="" />
+        </fieldset>
+        <fieldset>
+            <legend>Education</legend>
+            <label htmlFor="">School:</label>
+            <input type="text" name="" id="" />
+
+            <label htmlFor="">Title:</label>
+            <input type="text" name="" id="" />
+            
+            <label htmlFor="">Start Date:</label>
+            <input type="date" name="" id="" />
+            <label htmlFor="">End Date"</label>
+            <input type="date" name="" id="" />
+        </fieldset>
+        <fieldset>
+            <legend>Experience</legend>
+            <label htmlFor="">Company:</label>
+            <input type="text" name="" id="" />
+
+            <label htmlFor="">Title</label>
+            <input type="text" name="" id="" />
+            
+            <label htmlFor="">Start Date:</label>
+            <input type="date" name="" id="" />
+
+            <label htmlFor="">End Date:</label>
+            <input type="date" name="" id="" />
+
+            <label htmlFor="">Responsibilities:</label>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
         </fieldset>
         </>
     )
